66c25d76ff5fb8ebdee96e4c5a2e80a1532b8baa
yangjacomputa <topological@naver.com>
2020-05-13 06:45:04 +0900

    startup code for logicalYou

--- app/src/main/java/com/yangcom/logical/MainActivity.java
+++ app/src/main/java/com/yangcom/logical/MainActivity.java
@@ -1,14 +1,56 @@
 package com.yangcom.logical;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+    private CalmFragment fragmentCalm;
+    private IntentFragment fragmentIntent;
+    private ThirdFragment fragmentThird;
+    private SimulateFragment fragmentSim;
+    private FragmentTransaction transaction;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+
+        fragmentCalm = new CalmFragment();
+        fragmentIntent = new IntentFragment();
+        fragmentThird = new ThirdFragment();
+        fragmentSim = new SimulateFragment();
+
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragmentIntent).commitAllowingStateLoss();
+    }
+
+    public void clickHandler(View view)
+    {
+        transaction = fragmentManager.beginTransaction();
+
+        switch(view.getId())
+        {
+            case R.id.btn_fragmentCalm:
+                transaction.replace(R.id.frameLayout, fragmentCalm).commitAllowingStateLoss();
+                break;
+            case R.id.btn_fragmentIntent:
+                transaction.replace(R.id.frameLayout, fragmentIntent).commitAllowingStateLoss();
+                break;
+            case R.id.btn_fragmentThird:
+                transaction.replace(R.id.frameLayout, fragmentThird).commitAllowingStateLoss();
+                break;
+            case R.id.btn_fragmentSimulation:
+                transaction.replace(R.id.frameLayout, fragmentSim).commitAllowingStateLoss();
+                break;
+        }
     }
 }
